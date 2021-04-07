9413e9af1a697fceccd7b5e4a05024360f8168d8
Aditya Bhatt <77855132+aditya600@users.noreply.github.com>
2021-04-07 11:42:19 -0400

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-https.get('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json', (resp) => {
+https.get('https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/WDDGF81X39F247455?format=json', (resp) => {
     let data = '';
 
     // A chunk of data has been received.
@@ -13,23 +13,6 @@ https.get('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format
         console.log(JSON.parse(data));
     });
 
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
-});
-
-
-https.get('https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/mercedes?format=json', (resp) => {
-    let data = '';
-
-    // A chunk of data has been received.
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        console.log(JSON.parse(data));
-    });
 
 }).on("error", (err) => {
     console.log("Error: " + err.message);
